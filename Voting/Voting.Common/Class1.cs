99c240e276a160b5cd9addc3c56940d466119d72
linamarcela289 <linagaleano0@@gmail.com>
2019-03-14 12:53:50 -0500

    Structure

---
+++ Voting/Voting.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Voting.Common
+{
+    public class Class1
+    {
+    }
+}
---
+++ Voting/Voting.UIClassic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Voting.UIClassic
+{
+    public class Class1
+    {
+    }
+}
