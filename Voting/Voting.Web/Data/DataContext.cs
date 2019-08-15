56918acb3e7f2d4b33a55fe4a474a3f812dea277
Lina Marcela Galeano Ramirez <linagaleano0@gmail.com>
2019-08-15 15:03:49 -0500

    Create Entities

--- Voting/Voting.Web/Data/DataContext.cs
+++ Voting/Voting.Web/Data/DataContext.cs
@@ -4,6 +4,7 @@
     using Microsoft.EntityFrameworkCore;
     using Entities;
     using System.Linq;
+    using LeasingHome.Web.Data.Entities;
 
     public class DataContext : IdentityDbContext<User>
     {
@@ -12,6 +13,21 @@
        public DbSet<Vote> Votes { get; set; }
        public DbSet<Country> Countries { get; set; }
        public DbSet<City> Cities { get; set; }
+
+
+       public DbSet<Contract> Contracts { get; set; }
+
+       public DbSet<Lessee> Lessees { get; set; }
+
+       public DbSet<Owner> Owners { get; set; }
+
+       public DbSet<Property> Properties { get; set; }
+
+       public DbSet<PropertyImage> PropertyImages { get; set; }
+
+       public DbSet<PropertyType> PropertyTypes { get; set; }
+
+
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
 
